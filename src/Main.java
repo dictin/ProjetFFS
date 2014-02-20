d1d18ae1adc1d11e3f81173dfd5b16ac1483fb00
Dictin <link_1961@hotmail.com>
2014-02-20 10:37:04 -0500

    Reset the "Main" class to "Hello World"

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Testing gitingnore");
-        System.out.println("2nd test for .gitignore");
-        System.out.println("3rd test");
-        System.out.println("4th");
-        System.out.println("5th");
-        System.out.println("6th");
-        System.out.println("7th");
-        System.out.println("8th");
     }
 }
