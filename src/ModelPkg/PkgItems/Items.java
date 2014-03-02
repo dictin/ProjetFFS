6d9b45d009f5f0c9f23d9f9e6485b67a241c9933
Dictin <link_1961@hotmail.com>
2014-03-02 17:04:10 -0500

    Continuation du développement du système d'items. (En développement)

--- src/ModelPkg/PkgItems/Items.java
+++ src/ModelPkg/PkgItems/Items.java
@@ -5,11 +5,56 @@ package ModelPkg.PkgItems;
  */
 public enum Items {
 
-    POTION_1(PermBoostEffect.LOW_ATK_BOOST);
+    //Équipement permanent qui peut être installé entre les niveaux pour un boost constant
+
+    GENMOD_HP_0(0, "Coagulation rapide", PermBoostEffect.LOW_HP_BOOST),
+    GENMOD_HP_1(1, "Redondance organique", PermBoostEffect.MED_HP_BOOST),
+    GENMOD_HP_2(2, "R\u00E9g\u00E9n\u00E9r\u00E9scence c\u00E9lulaire", PermBoostEffect.HIGH_HP_BOOST),
+    GENMOD_SPD_3(3,"Syst\u00E8me nerveux autonomique", PermBoostEffect.LOW_SPD_BOOST),
+    GENMOD_SPD_4(4, "Pivot d'articulation ind\u00E9pendant", PermBoostEffect.MED_SPD_BOOST),
+    GENMOD_SPD_5(5, "Mouvement bip\u00E9dal", PermBoostEffect.HIGH_SPD_BOOST),
+    GENMOD_ATK_6(6, "Griffes perforantes", PermBoostEffect.LOW_ATK_BOOST),
+    GENMOD_ATK_7(7, "Enzymes Anticoagullants", PermBoostEffect.MED_ATK_BOOST),
+    GENMOD_ATK_8(8, "Machoire renforc\u00E9e", PermBoostEffect.HIGH_ATK_BOOST),
+
+
+    /*
+
+    LOW_ATK_BOOST(2, 1),
+    MED_ATK_BOOST(2, 3),
+    HIGH_ATK_BOOST(2, 5),
+
+    LOW_SMELLSENS_BOOST(3, 1),
+    MED_SMELLSENS_BOOST(3, 2),
+    HIGH_SMELLSENS_BOOST(3, 3),
+
+    LOW_SMELLSTR_BOOST(4, 5),
+    MED_SMELLSTR_BOOST(4, 10),
+    HIGH_SMELLSTR_BOOST(4, 20),
+    SUPER_SMELLSTR_BOOST(4, 30),
+
+    LOW_DEF_BOOST(5, 1),
+    MED_DEF_BOOST(5, 3),
+    HIGH_DEF_BOOST(5, 5),
+
+    LOW_END_BOOST(6, 10),
+    MED_END_BOOST(6, 20),
+    HIGH_END_BOOST(6, 30),
+
+    LOW_GBQT_BOOST(7, 10),
+    MED_GBQT_BOOST(7, 20),
+    HIGH_GBQT_BOOST(7, 30),
+    SUPER_GBQT_BOOST(7, 50);
+     */
 
 
 
-    private Items(ItemEffect effect){
+
+    private int itemID;
+    private String name;
+    private ItemEffect effect;
+
+    private Items(int itemID, String name, ItemEffect effect){
 
     }
 
