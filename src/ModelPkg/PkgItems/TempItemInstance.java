c36ebb910beddb9d53104fd08a49fbd2f0a0db1b
Dictin <link_1961@hotmail.com>
2014-03-03 16:02:52 -0500

    Finished the item framework overhaul

---
+++ src/ModelPkg/PkgItems/TempItemInstance.java
@@ -0,0 +1,24 @@
+package ModelPkg.PkgItems;
+
+public class TempItemInstance {
+
+    private ItemEffect itemEffect;
+    private int duration;
+
+    public TempItemInstance(BoostEffect effect){
+        this.itemEffect = effect;
+        this.duration = effect.getDuration();
+    }
+
+    public void activate(){
+        this.itemEffect.activate();
+    }
+
+    public void turn(){
+        duration--;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+}
