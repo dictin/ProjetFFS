205be5d6d939c56181c3884648f2709c47007643
Dictin <link_1961@hotmail.com>
2014-03-10 16:49:44 -0400

    Map and Coords update.
    Finished the map initialisation method.
    Finished the subsection return method
    Started the implementation of WildObjects on the map.

--- src/ModelPkg/MapData.java
+++ src/ModelPkg/MapData.java
@@ -1,17 +1,63 @@
 package ModelPkg;
 
-import java.util.ArrayList;
+import java.awt.*;
+import java.util.Random;
 
 public class MapData {
 
-    private ArrayList<Case> caseMap = new ArrayList<Case>();
+    private Case[][] map = new Case[30][30];
+
 
     public MapData(){
         this.initialize();
     }
 
-    private void initialize() {
-                              //TODO algorithme d'initialisation
+    private void initialize() { //8% arbre 2% roche 3% eau 2% trou
+        Random random = new Random(100);
+        int maxLength = 30;
+        int hivePosition = maxLength/2;
+        for (int i = 0; i < maxLength; i++){
+            for(int j = 0; j < maxLength; j++){
+                int caseType = random.nextInt();
+                if (caseType < 85){
+                    map[i][j] = new Case(new Point(i,j), null, new WildObject(0, true));
+                }else if (caseType < 93){
+                    map[i][j] = new Case(new Point(i,j), null, new WildObject(2, true));
+                }else if (caseType < 96){
+                    map[i][j] = new Case(new Point(i,j), null, new WildObject(3, true));
+                }else if (caseType < 98){
+                    map[i][j] = new Case(new Point(i,j), null, new WildObject(1, true));
+                }else if (caseType < 100){
+                    map[i][j] = new Case(new Point(i,j), null, new WildObject(4, true));
+                }else{
+                    map[i][j] = new Case(new Point(i,j), null, new WildObject(0, true));
+                }
+
+            }
+        }
+
+        map[hivePosition][hivePosition] = new Case(new Point(hivePosition,hivePosition), null, new WildObject(5, true));
+        map[hivePosition+1][hivePosition] = new Case(new Point(hivePosition+1,hivePosition), null, new WildObject(5, true));
+        map[hivePosition][hivePosition+1] = new Case(new Point(hivePosition,hivePosition+1), null, new WildObject(5, true));
+        map[hivePosition+1][hivePosition+1] = new Case(new Point(hivePosition+1,hivePosition+1), null, new WildObject(5, true));
+
+
+    }
+
+    public Case[][] getSubsection(Point origin, int radius){
+        Case[][] returnArray = new Case[(2*radius)+1][(2*radius)+1];
+        int subI = 0;
+        int subJ = 0;
+
+        for (int i = origin.x-radius; i <= origin.x+radius; i++){
+            for (int j = origin.y-radius; j <= origin.y+radius; j++){
+                returnArray[subI][subJ] = map[i][j];
+                subJ++;
+            }
+            subI++;
+        }
+
+        return returnArray;
     }
 
 }
--- src/ModelPkg/WildObject.java
+++ src/ModelPkg/WildObject.java
@@ -1,13 +1,35 @@
 package ModelPkg;
 
-import java.awt.*;
+public class WildObject {
 
-public abstract class WildObject {
+    private boolean traversable;
+    private int type = 0;
+    /*
+     * 1: Rock  - Climb
+     * 2: Tree  - Impassable
+     * 3: Water - Swim
+     * 4: Hole  - Fly
+     * 5: Hive  - Impassable
+     *
+     * 0: Nothing
+     *
+     * -1: Corpse
+     * -2: Food
+     *
+     */
 
-    private Point position;
 
-    public WildObject(Point position){
-        this.position = position;
+    public WildObject(int type, boolean traversable){
+        this.type = type;
+        this.traversable = traversable;
+    }
+
+    public boolean isTraversable() {
+        return traversable;
+    }
+
+    public int getType() {
+        return type;
     }
 
 }
