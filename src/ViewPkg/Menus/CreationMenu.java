546df3b76e3984b0820c09ce6d70bf2e10d116d3
ADogWithAMoustache <Chloe_ML_11@hotmail.com>
2014-03-31 16:27:25 -0400

    plusieurs menu sur vue :DDDD
    Ch

--- src/ViewPkg/Menus/CreationMenu.java
+++ src/ViewPkg/Menus/CreationMenu.java
@@ -12,11 +12,20 @@ public class CreationMenu extends ContextualMenu {
 
     public CreationMenu(MasterController controller){
         super(controller, "creation_menu");
-        JLabel background = new JLabel("Creation!");
+        JLabel background = new JLabel();
+        JButton fourmilier1 = new JButton(":D");
+        JButton fourmilier2 = new JButton(":)");
+        JButton fourmilier3 = new JButton("^^");
+        JButton fourmilier4 = new JButton(";)");
+        fourmilier1.setSize(100,100);
+        fourmilier1.setLocation(15,15);
+        this.add(fourmilier1);
+
         background.setVisible(true);
         background.setOpaque(true);
+        background.setSize(300,800);
         background.setBackground(Color.ORANGE);
-        background.setLocation(5,5);
+        background.setLocation(10,10);
         this.add(background);
     }
 
