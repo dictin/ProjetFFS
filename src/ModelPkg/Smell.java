8921dafeff1ad242f6ab882405e7d7a27a169493
Nsyse <hot_wheel@hotmail.com>
2014-04-27 11:19:11 -0400

    Created an alternative branch for an alternative smell system

--- src/ModelPkg/Smell.java
+++ src/ModelPkg/Smell.java
@@ -1,100 +1,15 @@
 package ModelPkg;
 
-import java.util.ArrayList;
-import java.util.Iterator;
+import java.awt.*;
 
+/**
+ * Created by Xav on 27/04/14.
+ */
 public class Smell {
-
-    public static final int MAX_VALUE = 100;
+    private Point coords;
     private int intensity;
-    private ArrayList<SmellID> contributors = new ArrayList<SmellID>();
-    private int type;
-    /*
-      le type de senteur:
-      1-Nourriture
-      2-Ennemi
-      3-Fourmilier ami
-      4-Maison
-
-     */
-
-
-    public Smell(int intensity, long sourceID, int type){
-        this.contributors.add(new SmellID(sourceID,intensity));
-        this.type = type;
-    }
-
-    public void diminish(){
-        Iterator<SmellID> iterator = this.contributors.iterator();
-        while (iterator.hasNext()){
-            iterator.next().diminish();
-        }
-    }
-
-    public void increase(SmellID smellID){
-        Iterator<SmellID> iterator = this.contributors.iterator();
-        if (this.oldOrigin(smellID.getiD())){
-            while(iterator.hasNext()){
-                SmellID instSmell = iterator.next();
-                if (instSmell.getiD()== smellID.getiD()){
-                    instSmell.setIntensity(smellID.getIntensity());
-
-                }
-            }
-
-        }else{
-            this.contributors.add(smellID);
-        }
-    }
-
-    public void clearOldSmell(){
-        Iterator<SmellID> iterator = this.contributors.iterator();
-        while(iterator.hasNext()){
-            SmellID instSmell = iterator.next();
-            if (instSmell.getIntensity() == 0){
-                this.contributors.remove(instSmell);   //TODO  UNSTABLE: Check for concurrent exception
-            }
-        }
-    }
-
-    public boolean oldOrigin(long iD){
-        boolean returnValue = false;
-        Iterator<SmellID> iterator = this.contributors.iterator();
-
-        while(iterator.hasNext()){
-            if (iterator.next().getiD() == iD){
-                returnValue = true;
-            }
-        }
-
-        return returnValue;
-
-    }
-
-    public int totalSmell(){
-        int total = 0;
-        Iterator<SmellID> iterator = this.contributors.iterator();
-
-        while(iterator.hasNext()){
-            total+=iterator.next().getIntensity();
-        }
-
-        if (total > 100){
-            total = 100;
-        }
-
-        return total;
-
-    }
-
-
-
-    public int getIntensity() {
-        return intensity;
-    }
-
-
-    public int getType() {
-        return type;
+    private int team;
+    public enum SmellType2{
+        food, animal, foe, item;
     }
 }
---
+++ src/ModelPkg/SmellSource.java
@@ -0,0 +1,16 @@
+package ModelPkg;
+
+import java.awt.*;
+
+/**
+ * Created by Xav on 27/04/14.
+ */
+public class SmellSource {
+        private Point coords;
+        private int intensity;
+        private int team;
+        public enum SmellType2{
+            food, animal, foe, item;
+        }
+}
+
